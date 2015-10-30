3ca94478d09521940e87c3f61251d6469889b345
Cindy Yu <cindy.yu.cy@gmail.com>
2015-10-30 13:04:03 -0700

    wrapper status

--- sample.sh
+++ sample.sh
@@ -1,10 +1,13 @@
 curl -H "Content-Type: application/json" -X POST -d \
 	'{
-		"subject": "hey sup",
-		"attachments": [
+	    "from": "90si", 
+	    "to": "cindy.yu.cy@gmail.com", 
+	    "subject": "SUBJECT", 
+	    "text": "Hello world ✔", 
+	    "attachments": [
 			{
 				"filename": "license.txt",
-      	"path": "https://raw.github.com/andris9/Nodemailer/master/LICENSE"
-      }
+	      		"path": "https://raw.github.com/andris9/Nodemailer/master/LICENSE"
+	      	}
 		]
 	 }' http://localhost:3131/email
--- server.js
+++ server.js
@@ -17,94 +17,9 @@ router.post('/email', koaBody,
     var subject = this.request.body.subject;
     var attachments = this.request.body.attachments;
 
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-      to: 'cindy.yu.cy@gmail.com', // list of receivers
-      subject: subject, // Subject line
-      text: 'Hello world ✔', // plaintext body
-      attachments: attachments
-    };
+    console.log(this.request.body);
 
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-        return console.log(error);
-      }
-      console.log('Message sent: ' + info.response);
-    });
-
-
-    this.body = JSON.stringify(this.request.body) + "\n";
-  }
-);
-
-router.post('/shipments/pickup', koaBody,
-  function *(next) {
-    var subject = this.request.body.subject;
-    var attachments = this.request.body.attachments;
-    var location = this.request.body.location;
-    var date = this.request.body.date;
-    var time = this.request.body.time;
-
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-      to: 'cindy.yu.cy@gmail.com', // list of receivers
-      subject: subject, // Subject line
-      text: 'time: ' + time + ' date: ' + date + ' location: ' + location, // plaintext body
-      attachments: attachments
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-        return console.log(error);
-      }
-      console.log('Message sent: ' + info.response);
-    });
-
-
-    this.body = JSON.stringify(this.request.body) + "\n";
-  }
-);
-
-router.post('/shipments/shipped', koaBody,
-  function *(next) {
-    var subject = this.request.body.subject;
-    var attachments = this.request.body.attachments;
-    var tracking = this.request.body.tracking;
-    var deliverydate = this.request.body.deliverydate;
-
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-      to: 'cindy.yu.cy@gmail.com', // list of receivers
-      subject: subject, // Subject line
-      text: 'tracking: ' + tracking + ' deliverydate: ' + deliverydate, // plaintext body
-      attachments: attachments
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if(error){
-        return console.log(error);
-      }
-      console.log('Message sent: ' + info.response);
-    });
-
-
-    this.body = JSON.stringify(this.request.body) + "\n";
-  }
-);
-
-router.post('/shipments/received', koaBody,
-  function *(next) {
-    var subject = this.request.body.subject;
-    var attachments = this.request.body.attachments;
-    var pdf = this.request.body.pdf;
-
-    var mailOptions = {
-      from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-      to: 'cindy.yu.cy@gmail.com', // list of receivers
-      subject: subject, // Subject line
-      text: 'pdf: ' + pdf, // plaintext body
-      attachments: attachments
-    };
+    var mailOptions = this.request.body;
 
     transporter.sendMail(mailOptions, function(error, info){
       if(error){
